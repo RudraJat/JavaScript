6b90fd86cae636b16819ea97dc278be30765a781
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-10-29 07:49:44 +0530

    if else

--- undefined.js
+++ undefined.js
@@ -1,3 +1,8 @@
 console.log(a);// here "a" will get a keyword which behaves like a pleaceholder "undefined"
 var a =7;
 console.log(a);
+if(a===undefined){
+    console.log("a is undefined")
+}else{
+    console.log("a is not undefined")
+}
