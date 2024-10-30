8a80c18226f84e26ea51a2545b2493a9090058c3
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-10-30 08:59:47 +0530

    strictness of const and let

--- letandconst.js
+++ letandconst.js
@@ -1,5 +1,9 @@
 console.log(b);
 // console.log(a); here it will give error because it is not stored in global memory space
-let a =10;
+let a ;
+a=100;
 console.log(a);
 var b=19;
+// const x; it'll give an error because const have to declare and intialize at same place
+// x=1000;
+
