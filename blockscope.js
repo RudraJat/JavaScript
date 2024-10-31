a0a876a2f132ee048eb6a04e61328b83a1550d14
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-10-31 07:03:08 +0530

    shadowing

--- blockscope.js
+++ blockscope.js
@@ -1,3 +1,6 @@
+var a=100;
+let b=200;
+const c=300;
 {
     var a=10;
     let b=20;
@@ -7,6 +10,8 @@
     console.log(c);
 }
 console.log(a);
+console.log(b);
+console.log(c);
 
 // These both will give error because let and const cant be access outside block scope
 // console.log(b); 
