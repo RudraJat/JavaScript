676fd51cf84c0f089bdd9ecfd42dc7e6335bcfca
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-02 07:59:02 +0530

    deep inside closure

--- closures.js
+++ closures.js
@@ -1,10 +1,26 @@
-function x(){
-    var a=2;
-    function y(){
-        console.log(a)
+// function x(){
+//     var a=2;
+//     function y(){
+//         console.log(a)
+//     }
+//     return y;
+// }
+// var c=x();
+// console.log(c);// it will return the whole fucniton y 
+
+// //Here it still remember that where is 'a' in the code and we can use that fucntion anywhere in the code
+// c();//it'll return the value of a, even when it is inside a local funtion this is due to closure 
+
+
+function z(){
+    var b=299;
+    function x(){
+        var a=1;
+        function y(){
+            console.log(a,b);
+        }
+        y();
     }
-    return y;
+    x();
 }
-var c=x();
-console.log(c);// it will return the whole fucniton y 
-c();//it'll return the value of a, even when it is inside a local funtion this is due to closure 
+z();
