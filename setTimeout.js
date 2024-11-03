e05d116876f3309669f4b7a3f11d9ca81c3553b3
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-03 06:50:33 +0530

    js  dont wait for anyone it goes on

--- setTimeout.js
+++ setTimeout.js
@@ -3,5 +3,6 @@ function s(){
     setTimeout(function(){
         console.log(i);
     },10000);
+    console.log("Namaste JS") //First it'll print this then it'll wait for 10 sec then it'll print valure of 'a'
 }
 s();
