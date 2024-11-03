acbbb2a3330d4b531e7293e05cb297ac8e08de89
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-03 07:03:29 +0530

    For loop with setTimeout

--- setTimeout.js
+++ setTimeout.js
@@ -6,3 +6,13 @@ function s(){
     console.log("Namaste JS") //First it'll print this then it'll wait for 10 sec then it'll print valure of 'a'
 }
 s();
+
+//PRINT 1,2,3... AFTER 1sec,2sec and so on....
+function x(){
+    for(let i=1;i<=5;i++){
+        setTimeout(function(){
+            console.log(i);
+        },i*1000);
+    }
+}
+x();
