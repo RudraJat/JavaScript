b52a4e9980bb47233deb1e5a72d261f5670a1dd7
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-05 23:24:31 +0530

    function expression and statement

--- function.js
+++ function.js
@@ -1,16 +1,28 @@
-var x=7;
-console.log(x);
-a();
-b();
+// var x=7;
+// console.log(x);
+// a();
+// b();
+// function a(){
+//     var x=10;
+//     console.log(x);
+// }
+// function b(){
+//     var x=100;
+//     console.log(x);
+// }
+// function c(){
+//     console.log("Hello Rudra!!!");
+// }
+// c();
+
+//Function Statement or Function Declaration
 function a(){
-    var x=10;
-    console.log(x);
+    console.log("a is called");
 }
-function b(){
-    var x=100;
-    console.log(x);
+
+//Function Expression
+    var z=function (){
+    console.log("b is called");
 }
-function c(){
-    console.log("Hello Rudra!!!");
-}
-c();
+a();
+z();
