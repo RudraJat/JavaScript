427bf3864cf3014fda0ee00d98ba9b118a285e1f
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-06 08:39:19 +0530

    first class function

--- function.js
+++ function.js
@@ -14,6 +14,36 @@ var t=function (){
      
 }
 
+//Named function expression
+var m= function xyz(){
+    console.log("Named function expression");
+}
+m();
+//xyz(); we cant call it outside like this because xyz is in local scope
+
+//First Class function
+//ability to pass function as a value
+function l (param1){
+    console.log(param1);
+}
+l(function(){
+
+});
+//another way
+function k(param1){
+    console.log(param1);
+}
+function xyz(){
+
+}
+k(xyz);
+//we can return a fucntion from a function
+function g(){
+    return function(){
+
+    }
+}
+console.log(g());
 // var x=7;
 // console.log(x);
 // a();
