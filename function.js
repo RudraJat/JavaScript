191532d666edb6def44093651794f5edce2c4582
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-07 09:51:40 +0530

    return a function

--- function.js
+++ function.js
@@ -1,49 +1,49 @@
-function a(){
-    console.log("a is called");
-}
+// function a(){
+//     console.log("a is called");
+// }
 
-//Function Expression
-    var z=function (){
-    console.log("b is called");
-}
-a();
-z();
+// //Function Expression
+//     var z=function (){
+//     console.log("b is called");
+// }
+// a();
+// z();
 
-//Anonymous Function
-var t=function (){
+// //Anonymous Function
+// var t=function (){
      
-}
+// }
 
-//Named function expression
-var m= function xyz(){
-    console.log("Named function expression");
-}
-m();
-//xyz(); we cant call it outside like this because xyz is in local scope
+// //Named function expression
+// var m= function xyz(){
+//     console.log("Named function expression");
+// }
+// m();
+// //xyz(); we cant call it outside like this because xyz is in local scope
 
-//First Class function
-//ability to pass function as a value
-function l (param1){
-    console.log(param1);
-}
-l(function(){
+// //First Class function
+// //ability to pass function as a value
+// function l (param1){
+//     console.log(param1);
+// }
+// l(function(){
 
-});
-//another way
-function k(param1){
-    console.log(param1);
-}
-function xyz(){
+// });
+// //another way
+// function k(param1){
+//     console.log(param1);
+// }
+// function xyz(){
 
-}
-k(xyz);
-//we can return a fucntion from a function
-function g(){
-    return function(){
+// }
+// k(xyz);
+// //we can return a fucntion from a function
+// function g(){
+//     return function(){
 
-    }
-}
-console.log(g());
+//     }
+// }
+// console.log(g());
 // var x=7;
 // console.log(x);
 // a();
@@ -62,3 +62,12 @@ console.log(g());
 // c();
 
 //Function Statement or Function Declaration
+function x(){
+    var w=9;
+    function y(){
+        console.log(w);
+    }
+    return y;
+}
+var n= x();
+console.log(n);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,46 @@
-function x(){
-//     for(let i=1;i<=5;i++){
-//         setTimeout(function(){
-//             console.log(i);
-//         },i*1000);
+function a(){
+//     console.log("a is called");
+// }
+
+// //Function Expression
+//     var z=function (){
+//     console.log("b is called");
+// }
+// a();
+// z();
+
+// //Anonymous Function
+// var t=function (){
+     
+// }
+
+// //Named function expression
+// var m= function xyz(){
+//     console.log("Named function expression");
+// }
+// m();
+// //xyz(); we cant call it outside like this because xyz is in local scope
+
+// //First Class function
+// //ability to pass function as a value
+// function l (param1){
+//     console.log(param1);
+// }
+// l(function(){
+
+// });
+// //another way
+// function k(param1){
+//     console.log(param1);
+// }
+// function xyz(){
+
+// }
+// k(xyz);
+// //we can return a fucntion from a function
+// function g(){
+//     return function(){
+
 //     }
 // }
-// x();
+// console.log(g());
