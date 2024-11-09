7e4662d58af22f9379c5582dc3fd05e84c2479eb
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-09 10:53:06 +0530

    set Timeout have trust issues

--- setTimeout.js
+++ setTimeout.js
@@ -1,31 +1,39 @@
-function s(){
-    var i = 1;
-    setTimeout(function(){
-        console.log(i);
-    },10000);
-    console.log("Namaste JS") //First it'll print this then it'll wait for 10 sec then it'll print valure of 'a'
-}
-s();
+// function s(){
+//     var i = 1;
+//     setTimeout(function(){
+//         console.log(i);
+//     },10000);
+//     console.log("Namaste JS") //First it'll print this then it'll wait for 10 sec then it'll print valure of 'a'
+// }
+// s();
 
-//PRINT 1,2,3... AFTER 1sec,2sec and so on....
-// function x(){
-//     for(let i=1;i<=5;i++){
-//         setTimeout(function(){
-//             console.log(i);
-//         },i*1000);
+// //PRINT 1,2,3... AFTER 1sec,2sec and so on....
+// // function x(){
+// //     for(let i=1;i<=5;i++){
+// //         setTimeout(function(){
+// //             console.log(i);
+// //         },i*1000);
+// //     }
+// // }
+// // x();
+
+// //IF WE HAVE TO USE VAR IN PLACE OF LET
+// function a(){
+//     for(var j=1;j<=5;j++){
+//         function close (a){
+//             setTimeout(function(){
+//                 console.log(a);
+//             },a*1000);
+//         }
+//         close(j);
 //     }
 // }
-// x();
+// a();
 
-//IF WE HAVE TO USE VAR IN PLACE OF LET
-function a(){
-    for(var j=1;j<=5;j++){
-        function close (a){
-            setTimeout(function(){
-                console.log(a);
-            },a*1000);
-        }
-        close(j);
-    }
-}
-a();
+
+
+console.log("Start");
+setTimeout(function cb(){
+    console.log("Callback");
+},5000);
+console.log("End");
