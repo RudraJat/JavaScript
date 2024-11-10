98a9586d9c1a12451830e8e654b6dd28a28798cf
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-10 10:16:47 +0530

    area, circumference, diameter

--- higherorderfunc.js
+++ higherorderfunc.js
@@ -8,5 +8,30 @@ const calculateArea= function(radius){
         output.push(Math.PI*radius[i]*radius[i]);
     }
     return output;
-}
+};
 console.log(calculateArea(radius));
+
+//2nd way it is reusable way
+const area=function(radius){
+    return Math.PI * radius*radius;
+};
+const circumferenece=function(radius){
+    return 2*Math.PI *radius;
+};
+const diameter=function(radius){
+    return 2*radius;
+};
+
+const calculate=function(radius,logic){
+    const output=[];
+    for(let i=0; i<radius.length;i++){
+        output.push(logic(radius[i]));
+    }
+    return output;
+}
+console.log("Area with another way");
+console.log(calculate(radius,area));
+console.log("CIRCUMFERENCE: ");
+console.log(calculate(radius,circumferenece));
+console.log("DIAMETER: ");
+console.log(calculate(radius,diameter));
