cee40c4a96d095ca45c681c9b57f621c28f6236e
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-12 11:15:39 +0530

    pritn data inside promise state

--- promises.js
+++ promises.js
@@ -1,3 +1,6 @@
 const github_api="https://api.github.com/users/RudraJat";
 const user =fetch(github_api);
-console.log(user)
+console.log(user);
+user.then(function(data){
+    console.log(data);
+})
