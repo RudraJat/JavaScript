05170c9ef4115d148eb2d4dcb5f6c91f818747f2
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-14 00:17:09 +0530

    error handling

--- promises.js
+++ promises.js
@@ -7,32 +7,60 @@
 
 //CREATING OUR OWN PROMISE
 
+
 const cart=["shoes", "shirt", "kurta"];
 
 const promise=createOrder(cart); //OrdreID
-console.log(promise);
-promise.then(function(orderId){
+
+promise.then(function(orderId){   // IT's FOR THE SUCCESS OR RESOLVE PART
     console.log(orderId);
+})
+.catch(function(err){  // IT's FOR THE FAILURE OR REJECTED PART
+    console.log(err.message);
 });
 
 //CODE FOR PRODUCER END HERE WE WILL CREATE PROMISE
 
+//which will get resolve
+
+// function createOrder(cart){
+//     const pr =new Promise(function(resolve, reject){
+//         if(!validateCart(cart)){
+//             //throw error if item is not in cart
+//             const err= new Error("Cart is not valid");
+//             reject(err);
+//         }
+//         const orderId="12345";
+//         if(orderId){
+//             setTimeout(function(){
+//                 resolve(orderId);
+//             },5000);
+//         }
+//     });
+//     return pr;
+// }
+// function validateCart(cart){
+//     return true;
+// };
+
+//which will get reject
+
 function createOrder(cart){
-    const pr =new Promise(function(resolve, reject){
-        if(!validateCart(cart)){
-            //throw error if item is not in cart
-            const err= new Error("Cart is not valid");
-            reject(err);
-        }
-        const orderId="12345";
-        if(orderId){
-            setTimeout(function(){
-                resolve(orderId);
-            },5000);
-        }
-    });
-    return pr;
-}
-function validateCart(cart){
-    return true;
-};
+     const pr =new Promise(function(resolve, reject){
+         if(!validateCart(cart)){
+             //throw error if item is not in cart
+             const err= new Error("Cart is not valid");
+             reject(err);
+         }
+         const orderId="12345";
+         if(orderId){
+             setTimeout(function(){
+                 resolve(orderId);
+             },5000);
+         }
+     });
+     return pr;
+ }
+ function validateCart(cart){
+     return false;
+ };
