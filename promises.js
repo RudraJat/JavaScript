15f9e1637e6601430966b4577347f36a796e4999
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-13 23:33:01 +0530

    creating our own promise

--- promises.js
+++ promises.js
@@ -1,6 +1,36 @@
-const github_api="https://api.github.com/users/RudraJat";
-const user =fetch(github_api);
-console.log(user);
-user.then(function(data){
-    console.log(data);
-})
+// const github_api="https://api.github.com/users/RudraJat";
+// const user =fetch(github_api);
+// console.log(user);
+// user.then(function(data){
+//     console.log(data);
+// })
+
+//CREATING OUR OWN PROMISE
+
+const cart=["shoes", "shirt", "kurta"];
+
+const promise=createOrder(cart); //OrdreID
+
+promise.then(function(orderId){
+    console.log(orderId);
+});
+
+//CODE FOR PRODUCER END HERE WE WILL CREATE PROMISE
+
+function createOrder(cart){
+    const pr =new Promise(function(resolve, reject){
+        if(!validateCart(cart)){
+            //throw error if item is not in cart
+            const err= new Error("Cart is not valid");
+            reject(err);
+        }
+        const orderId="12345";
+        if(orderId){
+            resolve(orderId);
+        }
+    });
+    return pr;
+}
+function validateCart(cart){
+    return true;
+};
