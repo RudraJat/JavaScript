b2c38431a62c91c8227b776f9d76d5dbc36e0541
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-19 22:11:01 +0530

    this in object

--- this.js
+++ this.js
@@ -8,4 +8,13 @@ function x(){
     console.log(this);
 }
 x();
-window.x();
+// window.x(); this keyword value depends on how it is called
+
+//this inside  a object's method
+const obj = {
+    a:10,
+    x:function() {
+        console.log(this);
+    },
+};
+obj.x();
