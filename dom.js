5b5cb1bf8b8cbb54df2ebfa9ec13aca787ede446
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-22 20:52:46 +0530

    commit

--- dom.js
+++ dom.js
@@ -11,9 +11,11 @@ console.log(xyz);
 console.dir(xyz);
 
 //FOR FIRST ELEMENT
+
 const FirstEl=document.querySelector("p");
 console.dir(FirstEl);
 
 //FOR ALL ELEMENT IN FORM OF NODE LIST
+
 const AllEl=document.querySelectorAll("p");
 console.dir(AllEl);
